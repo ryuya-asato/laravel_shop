211aa68079d06f95ae2e93c4736f248c7684e25d
e21001 <e21001@std.it-college.ac.jp>
2022-02-02 22:08:32 +0900

    softDelete

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -74,7 +74,10 @@ class OwnersController extends Controller
 
         return redirect()
         ->route('admin.owners.index')
-        ->with('message', 'オーナー登録を実施しました');
+        ->with([
+            'message' => 'オーナー登録を実施しました',
+            'status' => 'info'
+        ]);
     }
 
     /**
@@ -118,7 +121,10 @@ class OwnersController extends Controller
 
         return redirect()
         ->route('admin.owners.index')
-        ->with('message', 'オーナー情報を更新しました。');
+        ->with([
+            'message' =>'オーナー情報を更新しました。',
+            'status' => 'info'
+        ]);
     }
 
     /**
@@ -129,6 +135,25 @@ class OwnersController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Owner::findOrFail($id)->delete(); // ソフトデリート
+
+        return redirect()
+        ->route('admin.owners.index')
+        ->with([
+            'message' => 'オーナー情報を削除しました。',
+            'status' => 'alert'
+        ]);
+    }
+
+    public function expiredOwnersIndex()
+    {
+        $expiredOwners = Owner::onlyTrashed()->get();
+        return view('admin.expired-owners', compact('expiredOwners'));
+    }
+
+    public function expiredOwnersDestroy($id)
+    {
+        Owner::onlyTrashed()->findOrFail($id)->forceDelete();
+        return redirect()->route('admin.expired-owners.index');
     }
 }
