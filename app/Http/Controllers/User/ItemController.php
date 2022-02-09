8adc9f92760d42e0c5851dec31b3439190804128
e21001 <e21001@std.it-college.ac.jp>
2022-02-09 15:25:42 +0900

    検索フォーム　カテゴリー

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Stock;
 use Illuminate\Support\Facades\DB;
+use App\Models\PrimaryCategory;
+
 
 
 class ItemController extends Controller
@@ -30,10 +32,14 @@ class ItemController extends Controller
     public function index(Request $request)
     {
         $products = Product::availableItems()
+        ->selectCategory($request->category ?? '0')
         ->sortOrder($request->sort)
         ->paginate($request->pagination ?? '20');
 
-        return view('user.index', compact('products'));
+        $categories = PrimaryCategory::with('secondary')
+        ->get();
+
+        return view('user.index', compact('products', 'categories'));
     }
 
     public function show($id)
