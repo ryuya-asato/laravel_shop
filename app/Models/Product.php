ee3e88f8d47032a4a1d9ce36fc1a0296714b0862
e21001 <e21001@std.it-college.ac.jp>
2022-02-09 15:35:52 +0900

    検索フォーム　キーワード

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -119,4 +119,19 @@ class Product extends Model
 
         }
     }
+
+    public function scopeSearchKeyword($query, $keyword){
+        if(!is_null($keyword)){
+            // 全角スペースを半角に
+            $spaceConvert = mb_convert_kana($keyword, 's');
+            // 空白で区切る
+            $keywords = preg_split('/[\s]+/', $spaceConvert, -1, PREG_SPLIT_NO_EMPTY);
+            foreach($keywords as $word){
+                $query->where('products.name', 'like', '%'.$word.'%');
+            }
+            return $query;
+        } else {
+            return;
+        }
+    }
 }
