a87a9bb2e7c54284edcdc84ae4ac5b2cb961b580
e21001 <e21001@std.it-college.ac.jp>
2022-02-10 15:41:42 +0900

    オーナー向けメール送信

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -10,6 +10,7 @@ use App\Models\Stock;
 use Illuminate\Support\Facades\Auth;
 use App\Services\CartService;
 use App\Jobs\SendThanksMail;
+use App\Jobs\SendOrderedMail;
 
 
 class CartController extends Controller
@@ -56,13 +57,6 @@ class CartController extends Controller
 
     public function checkout()
     {
-        $items = Cart::where('user_id', Auth::id())->get();
-        $products = CartService::getItemInCart($items);
-        $user = User::findOrFail(Auth::id());
-
-        SendThanksMail::dispatch($products, $user);
-        dd('ユーザーメール送信テスト');
-
         $user = User::findOrFail(Auth::id());
         $products = $user->products;
 
@@ -111,6 +105,15 @@ class CartController extends Controller
 
     public function success()
     {
+        $items = Cart::where('user_id', Auth::id())->get();
+        $products = CartService::getItemInCart($items);
+        $user = User::findOrFail(Auth::id());
+
+        SendThanksMail::dispatch($products, $user);
+        foreach($products as $product){
+            SendOrderedMail::dispatch($product, $user);
+        }
+
         Cart::where('user_id', Auth::id())->delete();
 
         return redirect()->route('user.items.index');
